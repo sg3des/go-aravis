621c158ecacdd7993559b001cd6a6bc6ab7c38d1
Ron Evans <ron@hybridgroup.com>
2019-12-06 10:08:32 +0100

    device: add wrappers for get/set of string and int feature values

--- device.go
+++ device.go
@@ -4,7 +4,37 @@ package aravis
 // #include <arv.h>
 // #include <stdlib.h>
 import "C"
+import "unsafe"
 
 type Device struct {
 	device *C.struct__ArvDevice
+}
+
+func (d *Device) SetStringFeatureValue(feature, value string) {
+	cfeature := C.CString(feature)
+	cvalue := C.CString(value)
+	C.arv_device_set_string_feature_value(d.device, cfeature, cvalue)
+	C.free(unsafe.Pointer(cfeature))
+	C.free(unsafe.Pointer(cvalue))
+}
+
+func (d *Device) GetStringFeatureValue(feature string) (string, error) {
+	cfeature := C.CString(feature)
+	cvalue, err := C.arv_device_get_string_feature_value(d.device, cfeature)
+	C.free(unsafe.Pointer(cfeature))
+	return C.GoString(cvalue), err
+}
+
+func (d *Device) SetIntegerFeatureValue(feature string, value int) {
+	cfeature := C.CString(feature)
+	cvalue := C.int(value)
+	C.arv_device_set_integer_feature_value(d.device, cfeature, cvalue)
+	C.free(unsafe.Pointer(cfeature))
+}
+
+func (d *Device) GetIntegerFeatureValue(feature string) (int, error) {
+	cfeature := C.CString(feature)
+	cvalue, err := C.arv_device_get_integer_feature_value(d.device, cfeature)
+	C.free(unsafe.Pointer(cfeature))
+	return int(cvalue), err
 }
